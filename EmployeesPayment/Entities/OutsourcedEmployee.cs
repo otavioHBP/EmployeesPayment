51c492ee0ee787ce4107f2c15755ee6fa662fd7a
Otavio <Otavio@DESKTOP-UOM8MPN>
2023-07-24 18:31:25 -0300

    Adicionar arquivos de projeto.

---
+++ EmployeesPayment/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace EmployeesPayment.Entities 
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee(string? name)
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge ;
+
+        }
+
+    }
+}
